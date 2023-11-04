cbcb0ad774f1786202047f2c4da48e9cfd028a4d
Svetlin DImitrov <prolama6@gmail.com>
2023-11-04 12:01:23 +0200

    fix bug with validation

--- microServices/auth-service/src/main/java/org/auth/util/UserValidator.java
+++ microServices/auth-service/src/main/java/org/auth/util/UserValidator.java
@@ -5,6 +5,7 @@ import org.auth.model.dto.LoginUserDto;
 import org.auth.model.entity.User;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
@@ -13,11 +14,11 @@ import java.util.function.Predicate;
 public class UserValidator {
 
     public  Predicate<EditUserDto> validUsernameChange = (userDto -> userDto.getUsername() != null && userDto.getUsername().length() > 4 && !userDto.getUsername().isBlank());
-    public  Predicate<EditUserDto> validKilogramsChange = (userDto -> userDto.getKilograms() != null);
+    public  Predicate<EditUserDto> validKilogramsChange = (userDto -> userDto.getKilograms() != null && userDto.getKilograms().compareTo(new BigDecimal(5)) > 0);
     public  Predicate<EditUserDto> validWorkoutStateChange = (userDto -> userDto.getWorkoutState() != null);
     public  Predicate<EditUserDto> validHeightChange = (userDto -> userDto.getHeight() != null);
     public  Predicate<EditUserDto> validGenderChange = (userDto -> userDto.getGender() != null);
-    public  Predicate<EditUserDto> validAgeChange = (userDto -> userDto.getAge() != null);
+    public  Predicate<EditUserDto> validAgeChange = (userDto -> userDto.getAge() != null && userDto.getAge() > 0);
     public  Predicate<User> validateFullRegistration = (entity ->
             entity.getKilograms() != null &&
             entity.getWorkoutState() != null &&
