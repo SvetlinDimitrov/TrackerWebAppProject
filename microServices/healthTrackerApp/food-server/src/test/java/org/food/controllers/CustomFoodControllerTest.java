598a3aa8451bc34bdf3c6cc6a45f9c1dbee26e4a
Svetlin DImitrov <prolama6@gmail.com>
2023-12-08 17:58:35 +0200

    fix bug on food tests

--- microServices/healthTrackerApp/food-server/src/test/java/org/food/controllers/CustomFoodControllerTest.java
+++ microServices/healthTrackerApp/food-server/src/test/java/org/food/controllers/CustomFoodControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class CustomFoodControllerTest {
     
--- microServices/healthTrackerApp/food-server/src/test/java/org/food/controllers/FoodControllerTest.java
+++ microServices/healthTrackerApp/food-server/src/test/java/org/food/controllers/FoodControllerTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,6 +23,7 @@ import com.google.gson.Gson;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class FoodControllerTest {
 
     @Autowired
